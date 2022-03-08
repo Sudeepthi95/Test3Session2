a52735965096b2cf2b63260f4d91a07b58192559
sudeepthi95 <93108251+Sudeepthi95@users.noreply.github.com>
2022-03-08 06:04:56 +0000

    p2 done

--- p2final.c
+++ p2final.c
@@ -1,1 +1,47 @@
 #include<stdio.h>
+struct _fraction
+{
+int num,den;
+};
+typedef struct _fraction Fraction;
+
+Fraction input_fraction()
+{
+  Fraction f;
+  printf("Enter one fraction (NUM DEN):\n");
+  scanf("%d%d",&f.num,&f.den);
+  return f;
+}
+Fraction largest_fraction(Fraction f1,Fraction f2,Fraction f3)
+{
+  float a=(float)f1.num/f1.den;
+  float b=(float)f2.num/f2.den;
+  float c=(float)f3.num/f3.den;
+  if (a<b && a<c)
+  {
+    return f1;
+  }
+  else if (b<c)
+  {
+    return f2;
+  }  
+  else
+  {
+    return f3;
+  }
+}
+void output(Fraction f1,Fraction f2,Fraction f3,Fraction largest)
+{
+  printf("the smallest of %d/%d,%d/%d and %d/%d is %d/%d\n",f1.num,f1.den,f2.num,f2.den,f3.num,f3.den,largest.num,largest.den);
+}
+int main()
+{
+  Fraction f1 = input_fraction();
+  Fraction f2 = input_fraction();
+  Fraction f3 = input_fraction();
+  Fraction largest = largest_fraction(f1,f2,f3);
+  output(f1,f2,f3,largest);
+  return 0;
+}
+
+
